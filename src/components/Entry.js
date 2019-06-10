1ff96efe58be4babba35eb2623b8c95e1a89d7c4
ecshaff <emmyshaff@gmail.com>
2019-06-10 16:42:05 -0700

    like button added

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -8,6 +8,16 @@ import React , { Component } from 'react';
 // COMPONENT
 // =================
 class Entry extends Component {
+    constructor() {
+        super()
+        this.state = {
+            liked: false
+        }
+        this.toggleLike = this.toggleLike.bind(this);
+    }
+    toggleLike() {
+    this.setState({ liked: !this.state.liked });
+  }
     render() {
         return (
             <div className='entry-container'>
@@ -18,11 +28,15 @@ class Entry extends Component {
                     <h3>{ this.props.entry.status }</h3>
                     <p>{ this.props.entry.text }</p>
                     <img src={ this.props.entry.image } alt="not available"/>
+                    
+                    
                 </div>
 
                 <div className='entry-action'>
                     <button onClick={ () => this.props.handleDelete(this.props.entry.id, this.props.arrayIndex) }>Delete</button>
                     <button>Edit</button>
+                    <button onClick={this.toggleLike}>Like</button>
+                     { this.state.liked ? <h4>&hearts;</h4> : null }
                 </div>
             </div>
         );
